46094330ada7c77ee539c38ddc37335802096b91
Rareş-Theodor BOGDAN <rarestheodorbogdan@gmail.com>
2017-10-02 11:50:06 +0300

    Added EntityFramework

---
+++ MVC-App/Models/AppToDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class AppToDb : DbContext
+    {
+        public AppToDb() : base("name = TrainsConnection")
+        {
+
+        }
+        public DbSet<Train> Trains { get; set; }
+
+    }
+}
