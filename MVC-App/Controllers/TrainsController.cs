24cdb99023ff4f0bde25934c2ccf66871034e719
Rareş-Theodor BOGDAN <rarestheodorbogdan@gmail.com>
2017-10-02 15:11:09 +0300

    Added EntityFramework

---
+++ MVC-App/Controllers/TrainsController.cs
@@ -0,0 +1,102 @@
+﻿using MVC_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_App.Controllers
+{
+    public class TrainsController : Controller
+    {
+        private TrainsToDb _db = new TrainsToDb();
+
+        // GET: Trains
+        public ActionResult Index()
+        {
+            var model = _db.Trains.ToList();
+            return View();
+        }
+
+        // GET: Trains/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Trains/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trains/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Trains/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Trains/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Trains/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Trains/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if(_db == null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC-App/Models/TrainsToDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class TrainsToDb : DbContext
+    {
+        public DbSet<Train> Trains { get; set; }
+
+        public TrainsToDb() : base("server=TrainsDb")
+        {
+
+        }
+    }
+}
